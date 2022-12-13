4b42a1e728acfb037ddc2ea93a2b9f7ac9a79624
Alexandre GAUTHIER <al.gauthier@student.isartdigital.com>
2022-12-13 15:01:20 +0100

    fixed engine macros

--- AnimationProgramming/AnimationProgramming/AnimationProgramming/Macros.h
+++ AnimationProgramming/AnimationProgramming/AnimationProgramming/Macros.h
@@ -5,12 +5,12 @@
 #include "src/Transform.h"
 
 // return local transform (ie relative to the parent) of the bone in the bind pose (or rest pose, also called T-pose)
-void GetSkeletonBoneLocalBindTransform(int boneIndex, EmMaths::Float3& position, EmMaths::Quaternion& quat) { GetSkeletonBoneLocalBindTransform(boneIndex, position.x, position.y, position.z, quat.d, quat.a, quat.b, quat.c); };
+void GetSkeletonBoneLocalBindTransform(int boneIndex, EmMaths::Float3& position, EmMaths::Quaternion& quat) { GetSkeletonBoneLocalBindTransform(boneIndex, position.x, position.y, position.z, quat.a, quat.b, quat.c, quat.d); };
 
 void GetSkeletonBoneLocalBindTransform(int boneIndex, Transform& transform) { GetSkeletonBoneLocalBindTransform(boneIndex, transform.position, transform.rotation); };
 // return the transform relative to the bind pose
 // there are two animations named "ThirdPersonWalk.anim" and "ThiedPersonRun.anim"
-void GetAnimLocalBoneTransform(const char* animName, int boneIndex, int keyFrameIndex, EmMaths::Float3& transformPosition, EmMaths::Quaternion& transformQuat) { GetAnimLocalBoneTransform(animName, boneIndex, keyFrameIndex, transformPosition.x, transformPosition.y, transformPosition.z, transformQuat.d, transformQuat.a, transformQuat.b, transformQuat.c);};
+void GetAnimLocalBoneTransform(const char* animName, int boneIndex, int keyFrameIndex, EmMaths::Float3& transformPosition, EmMaths::Quaternion& transformQuat) { GetAnimLocalBoneTransform(animName, boneIndex, keyFrameIndex, transformPosition.x, transformPosition.y, transformPosition.z, transformQuat.a, transformQuat.b, transformQuat.c, transformQuat.d);};
 
 void GetAnimLocalBoneTransform(const char* animName, int boneIndex, int keyFrameIndex, Transform& transform) { GetAnimLocalBoneTransform(animName, boneIndex, keyFrameIndex, transform.position, transform.rotation); };
 
